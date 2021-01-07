47104a9fffeae56cf007af456e52820680dbdbb5
muraoka naohito <ciel.phantomhive.4869@gmail.com>
2021-01-07 22:51:00 +0900

    更新: main - Providerを追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:firebase_sample/pages/home_screen_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'constants/colors.dart';
+import 'models/switch_app_theme_provider.dart';
+import 'models/theme_provider.dart';
+
 void main() {
   runApp(
     MultiProvider(
@@ -15,6 +19,16 @@ void main() {
         ),
         ChangeNotifierProvider(
           create: (_) => HomeScreenNotifier(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ThemeProvider(
+            isLightTheme: true,
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => SwitchAppThemeProvider(
+            currentTheme: colorList[6],
+          ),
         ),
       ],
       child: MyApp(),
